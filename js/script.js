f2baf7e1bba4dc8a903d0ac743cf5745cfa2ba57
MRDarkstar99 <157112511+MRDarkstar99@users.noreply.github.com>
2024-01-19 19:24:24 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
+
+const sr = ScrollReaveal({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+});
+sr.reveal('.logo', { delay: 200, origin: 'left' })
